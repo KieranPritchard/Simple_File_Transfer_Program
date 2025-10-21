d6821c14e2039b24fc768834ea515652a5a00133
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-10-21 20:51:50 +0100

    added server connection stuff

--- client.py
+++ client.py
@@ -1,9 +1,15 @@
 import socket
 
 # sets up server connection
+host = "127.0.0.1"
+port = 12345
+server_information = (host, port)
 
+# Creates server socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Takes file path as an input
+file_path = input("Please input a file path: ")
 
 # Opens the file and stores the data in a variable
 
